50470a99d1bf6c49fc8c61a49549182a58cc4b32
G0V1NDS <connect.govinds@gmail.com>
2021-06-30 15:44:30 +0530

    Added utility to close floating window

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -117,4 +117,14 @@ M.wrap_lines = function(input, width)
     return output
 end
 
+M.close_floating_windows = function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local config = vim.api.nvim_win_get_config(win);
+        if config.relative ~= "" then
+            vim.api.nvim_win_close(win, false);
+            print('Closing window', win)
+        end
+    end
+end
+
 return M
