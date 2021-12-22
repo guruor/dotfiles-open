2199d971c4ebd2ced6b639836cad8580dfaa3a6e
G0V1NDS <connect.govinds@gmail.com>
2021-12-22 13:46:53 +0530

    Added notetaker script for yabai to open notes window Changed notetaker script type from sh to bash

---
+++ .config/yabai/scripts/open_notetaker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+window_name="Notes"
+terminal="alacritty"
+function open_notetaker() {
+    # Checking if note window is already open
+    note_taker_window=$(yabai -m query --windows | jq "map(select((.app == \"$terminal\") and .title == \"$window_name\"))[0] // empty");
+    if [[ -z "$note_taker_window" ]]; then
+        $terminal --title "$window_name" -e $SHELL -lc "notetaker";
+    else
+        # If window is already open either move it to current space or minimize it else focus it
+        window_id=$(echo $note_taker_window | jq '.id')
+        window_space_id=$(echo $note_taker_window | jq '.space')
+        current_space_id=$(yabai -m query --spaces --space | jq .index)
+        yabai -m window $window_id --space mouse
+        yabai -m window --focus $(echo $note_taker_window | jq '.id')
+
+        if [[ $(echo $note_taker_window | jq '."is-minimized"') == "false" ]]; then
+            echo -e "$current_space_id, $window_space_id"
+            if [[ $window_space_id == $current_space_id ]]; then
+                # Only minimize when window was on current space
+                yabai -m window $window_id --minimize
+                yabai -m window --focus mouse 2>/dev/null
+            fi
+        fi
+    fi
+}
+
+open_notetaker
