29a7adb17a82a864571c8a1575a82601167ce197
G0V1NDS <connect.govinds@gmail.com>
2022-02-13 17:36:55 +0530

    Added debug config for golang

--- .config/nvim/lua/dbg/init.lua
+++ .config/nvim/lua/dbg/init.lua
@@ -18,6 +18,41 @@ dap.adapters.nlua = function(callback, config)
     callback({type = 'server', host = config.host, port = config.port})
 end
 
+-- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+dap.adapters.go = function(callback, config)
+    local stdout = vim.loop.new_pipe(false)
+    local handle
+    local pid_or_err
+    local port = 38697
+    local opts = {
+      stdio = {nil, stdout},
+      args = {"dap", "-l", "127.0.0.1:" .. port},
+      detached = true
+    }
+    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+      stdout:close()
+      handle:close()
+      if code ~= 0 then
+        print('dlv exited with code', code)
+      end
+    end)
+    assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require('dap.repl').append(chunk)
+        end)
+      end
+    end)
+    -- Wait for delve to start
+    vim.defer_fn(
+      function()
+        callback({type = "server", host = "127.0.0.1", port = port})
+      end,
+      100)
+  end
+
 dap.configurations.python = {
     {
         type = 'python',
@@ -73,6 +108,40 @@ dap.configurations.rust = {
         -- But you should be aware of the implications:
         -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
         runInTerminal = false
+    }
+}
+
+
+dap.configurations.go = {
+    -- {
+      -- type = "go",
+      -- name = "gin_server",
+      -- request = "launch",
+      -- program = "${workspaceFolder}/cmd/gin_server",
+      -- env = {
+          -- ENV = "local"
+      -- }
+    -- },
+    {
+      type = "go",
+      name = "Debug",
+      request = "launch",
+      program = "${file}"
+    },
+    {
+      type = "go",
+      name = "Debug test", -- configuration for debugging test files
+      request = "launch",
+      mode = "test",
+      program = "${file}"
+    },
+    -- works with go.mod packages and sub packages
+    {
+      type = "go",
+      name = "Debug test (go.mod)",
+      request = "launch",
+      mode = "test",
+      program = "./${relativeFileDirname}"
     }
 }
 
