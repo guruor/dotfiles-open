fba3683947129000e3d05a418e68fe527bdc5612
G0V1NDS <connect.govinds@gmail.com>
2022-07-14 11:04:33 +0530

    Showing virtual_text in diagnostic Formatting changes

--- .config/nvim/lua/lsp/handlers.lua
+++ .config/nvim/lua/lsp/handlers.lua
@@ -1,17 +1,18 @@
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] = function(...)
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
-        {
-        underline = false,
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
         update_in_insert = false,
         signs = {
             severity_limit = 'Warning',
         },
-        virtual_text = false,
     })(...)
-    pcall(vim.lsp.diagnostic.setloclist, {open_loclist = false})
+    pcall(vim.diagnostic.setloclist, { open = false })
 end
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = vim.g.floating_window_border_dark})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = vim.g.floating_window_border_dark,
+})
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = vim.g.floating_window_border_dark})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = vim.g.floating_window_border_dark,
+})
