8bc43bbe3999952202a8129c4c5a6e7f458d86b6
G0V1NDS <connect.govinds@gmail.com>
2022-10-04 19:14:39 +0530

    Using --single-instance mode when opening kitty terminal

--- .config/yabai/scripts/open_notetaker.sh
+++ .config/yabai/scripts/open_notetaker.sh
@@ -8,7 +8,9 @@ function open_notetaker() {
     # Checking if note window is already open
     note_taker_window=$(yabai -m query --windows | jq "map(select((.app == \"$terminal\") and .title == \"$window_name\"))[0] // empty");
     if [[ -z "$note_taker_window" ]]; then
-        $terminal --title "$window_name" -e $SHELL -lc "notetaker; $SHELL";
+        # $terminal --title "$window_name" -e $SHELL -lc "notetaker; $SHELL";
+        # When opening kitty window, opening with single instance mode
+        $terminal -1 --title "$window_name" -e $SHELL -lc "notetaker; $SHELL";
     else
         # If window is already open either move it to current space or minimize it else focus it
         window_id=$(echo $note_taker_window | jq '.id')
