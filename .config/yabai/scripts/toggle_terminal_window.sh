6dc2b754040967eec3edd7828156efcfd8be165f
G0V1NDS <connect.govinds@gmail.com>
2022-10-16 16:38:56 +0530

    Generalized open_notetaker.sh script to toggle any terminal window

---
+++ .config/yabai/scripts/toggle_terminal_window.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Reference for floating window with yabai (https://github.com/Granitosaurus/bin/blob/master/dropdown)
+
+window_name="${1:=Scratchpad}"
+command_str="${2}"
+# terminal="alacritty"
+terminal="kitty"
+function toggle_window() {
+    # Checking if window is already open
+    term_window=$(yabai -m query --windows | jq "map(select((.app == \"$terminal\") and .title == \"$window_name\"))[0] // empty");
+    if [[ -z "$term_window" ]]; then
+        # $terminal --title "$window_name" -e $SHELL -lc "${command_str}; $SHELL";
+        # When opening kitty window, opening with single instance mode
+        $terminal -1 --title "$window_name" -e $SHELL -lc "${command_str}; $SHELL";
+    else
+        # If window is already open either move it to current space or minimize it else focus it
+        window_id=$(echo $term_window | jq '.id')
+        window_space_id=$(echo $term_window | jq '.space')
+        current_space_id=$(yabai -m query --spaces --space | jq .index)
+        yabai -m window $window_id --space mouse
+        yabai -m window --focus $(echo $term_window | jq '.id')
+
+        if [[ $(echo $term_window | jq '."is-minimized"') == "false" ]]; then
+            echo -e "$current_space_id, $window_space_id"
+            if [[ $window_space_id == $current_space_id ]]; then
+                # Only minimize when window was on current space
+                yabai -m window $window_id --minimize
+                yabai -m window --focus recent 2>/dev/null
+            fi
+        fi
+    fi
+}
+
+toggle_window
