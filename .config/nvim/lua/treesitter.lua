0b925f53bc5f5f8dcca867ff8b06501c89e3c736
G0V1NDS <connect.govinds@gmail.com>
2022-10-29 09:23:45 +0530

    Added custom treesitter parser for http files for better syntax for rest.nvim http files

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -29,3 +29,15 @@ require'nvim-treesitter.configs'.setup {
       },
 }
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+-- Custom tree-sitter parser for http files, grammar includes rest-nvim format
+-- https://github.com/rest-nvim/rest.nvim/issues/30
+parser_config.http = {
+  install_info = {
+    url = 'https://github.com/rest-nvim/tree-sitter-http',
+    revision = 'main',
+    files = { 'src/parser.c'},
+  },
+  filetype = 'http',
+}
+
