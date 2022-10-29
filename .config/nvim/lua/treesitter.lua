bad62c528158da93f1f2d40c3af6d2ab4f5b61cd
G0V1NDS <connect.govinds@gmail.com>
2022-10-29 09:22:40 +0530

    Added typescript parser

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -11,6 +11,7 @@ require'nvim-treesitter.configs'.setup {
       "python",
       "go",
       "javascript",
+      "typescript",
       "json",
       "markdown",
       "markdown_inline",
@@ -28,4 +29,3 @@ require'nvim-treesitter.configs'.setup {
       },
 }
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
