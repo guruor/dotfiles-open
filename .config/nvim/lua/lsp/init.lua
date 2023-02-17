f4b934889c8618e4c93b8c358e078b6f0b241b5e
G0V1NDS <connect.govinds@gmail.com>
2023-02-17 21:17:53 +0530

    Added mason.nvim for better installation of lsp servers

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -9,6 +9,38 @@ local M = {}
 -- Adding some lsp related config from kickstart.nvim
 -- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 require"fidget".setup{}
+require("mason").setup()
+-- Setup neovim lua configuration
+require('neodev').setup()
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config. You must look up that documentation yourself.
+local servers = {
+  clangd = {},
+  gopls = {},
+  pyright = {},
+  rust_analyzer = {},
+  vimls = {},
+  jsonls = {},
+  yamlls = {},
+  cssls = {},
+  html = {},
+  bashls = {},
+  dockerls = {},
+  terraformls = {},
+  tsserver = {},
+  lua_ls = {},
+  sqlls = {},
+  efm = {},
+  -- bufls = {},
+}
+
 vim.lsp.protocol.CompletionItemKind = {
     Text = " [text]",
     Method = " [method]",
@@ -86,6 +118,7 @@ vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnost
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 
 local on_attach = function(client)
@@ -106,6 +139,7 @@ function _G.activeLSP()
     end
     _G.P(servers)
 end
+
 function _G.bufferActiveLSP()
     local servers = {}
     for _, lsp in pairs(vim.lsp.buf_get_clients()) do
@@ -113,6 +147,21 @@ function _G.bufferActiveLSP()
     end
     _G.P(servers)
 end
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
+
 
 -- https://github.com/golang/tools/tree/master/gopls
 lspconfig.gopls.setup {
@@ -203,7 +252,8 @@ local function get_lua_runtime()
 
     return result
 end
-lspconfig.sumneko_lua.setup {
+
+lspconfig.lua_ls.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     cmd = {"lua-language-server"},
