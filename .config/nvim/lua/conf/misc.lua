9d5378ae46e0cb016b4982b55b5ba0967b593e41
G0V1NDS <connect.govinds@gmail.com>
2023-02-19 15:27:34 +0530

    Added smartcolumn plugin

--- .config/nvim/lua/conf/misc.lua
+++ .config/nvim/lua/conf/misc.lua
@@ -1,1 +1,8 @@
 require('Comment').setup()
+
+local smartcolumn_config = {
+   colorcolumn = 120,
+   disabled_filetypes = { "help", "text", "markdown" },
+}
+
+require("smartcolumn").setup(smartcolumn_config)
