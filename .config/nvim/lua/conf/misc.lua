1db8c1fdc81de1be3779396362647782c9a91cf6
G0V1NDS <connect.govinds@gmail.com>
2023-03-26 19:37:24 +0530

    Disable underline on context start for indent_blankline

--- .config/nvim/lua/conf/misc.lua
+++ .config/nvim/lua/conf/misc.lua
@@ -120,7 +120,7 @@ vim.g.indent_blankline_use_treesitter = true
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
     show_end_of_line = true,
     space_char_blankline = " ",
 }
