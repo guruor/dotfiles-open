9ca3d84d5a31c9d7675bf8c2fa92a45b0f439c30
G0V1NDS <connect.govinds@gmail.com>
2023-04-21 17:52:53 +0530

    Added chatgpt custom config

--- .config/nvim/lua/plugins/configs/misc.lua
+++ .config/nvim/lua/plugins/configs/misc.lua
@@ -86,4 +86,20 @@ M.blankline = {
   strict_tabs = true,
 }
 
+M.chatgpt = {
+  chat = {
+    keymaps = {
+      close = { "jk", "kj", "<Esc>" },
+      yank_last = "<C-y>",
+      scroll_up = "<C-u>",
+      scroll_down = "<C-d>",
+      toggle_settings = "<C-o>",
+      new_session = "<C-n>",
+      cycle_windows = "<Tab>",
+    },
+  },
+  popup_input = {
+    submit = "<C-s>",
+  },
+}
 return M
