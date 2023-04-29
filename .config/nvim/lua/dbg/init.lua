8fd4bc3dbc91fd337988965e9bafb25d6babdfee
G0V1NDS <connect.govinds@gmail.com>
2023-04-29 13:51:49 +0530

    Formatted file

--- .config/nvim/lua/dbg/init.lua
+++ .config/nvim/lua/dbg/init.lua
@@ -1,15 +1,14 @@
 -- https://alpha2phi.medium.com/neovim-dap-enhanced-ebc730ff498b
 -- :DIInstall jsnode_dbg
 -- :DIInstall go_delve_dbg
-local dap = require 'dap'
-require("mason-nvim-dap").setup({
-        ensure_installed = { "python", "delve", "codelldb", "js", "node2", "bash"},
-        automatic_setup = true,
-})
-
+local dap = require "dap"
+require("mason-nvim-dap").setup {
+  ensure_installed = { "python", "delve", "codelldb", "js", "node2", "bash" },
+  automatic_setup = true,
+}
 
 -- Setting log level possible values: TRACE, DEBUG, INFO, WARN, ERROR
-dap.set_log_level("TRACE")
+dap.set_log_level "TRACE"
 
 require("dbg.lua").setup(dap)
 require("dbg.python").setup(dap)
@@ -26,7 +25,7 @@ vim.g.dap_virtual_text = true
 -- Enable virutal text, requires rcarriga/nvim-dap-ui
 require("dapui").setup()
 -- Enable virutal text, requires mfussenegger/nvim-dap-python, overide it with .vscode/launch.json
-require('dap-python').setup(vim.fn.expand "~/.pyenv/versions/debugpy/bin/python")
+require("dap-python").setup(vim.fn.expand "~/.pyenv/versions/debugpy/bin/python")
 
 -- Loads configurations from vscode Launch.json, adding it at end so it extends the existing configuration
 require("dap.ext.vscode").load_launchjs()
