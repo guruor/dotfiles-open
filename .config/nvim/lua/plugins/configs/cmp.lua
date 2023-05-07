568e17cdbdd61ad08320fe98a90d18e92764b645
G0V1NDS <connect.govinds@gmail.com>
2023-05-07 16:44:36 +0530

    Formatted with stylua

--- .config/nvim/lua/plugins/configs/cmp.lua
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -1,121 +1,120 @@
-local lspkind = require('lspkind')
+local lspkind = require "lspkind"
 vim.o.completeopt = "menu,menuone,noselect"
 
 -- Set up nvim-cmp.
-local cmp = require 'cmp'
-local luasnip = require('luasnip')
+local cmp = require "cmp"
+local luasnip = require "luasnip"
 
-cmp.setup({
-    enabled = function()
-      return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+cmp.setup {
+  enabled = function()
+    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+  end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
     end,
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
-    },
-    formatting = {
-        fields = { 'menu', 'abbr', 'kind' },
-        format = lspkind.cmp_format({
-          mode = 'symbol', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+  },
+  formatting = {
+    fields = { "menu", "abbr", "kind" },
+    format = lspkind.cmp_format {
+      mode = "symbol", -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-          -- The function below will be called before any actual modifications from lspkind
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function (entry, vim_item)
-            vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snp]",
-                buffer = "[Buf]",
-                nvim_lua = "[Lua]",
-                path = "[Pth]",
-                ['vim-dadbod-completion'] = "[DB]",
-                calc = "[Clc]",
-                emoji = "[Emj]",
-            })[entry.source.name]
-            return vim_item
-          end
-        })
-      },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        vim_item.menu = ({
+          nvim_lsp = "[LSP]",
+          luasnip = "[Snp]",
+          buffer = "[Buf]",
+          nvim_lua = "[Lua]",
+          path = "[Pth]",
+          ["vim-dadbod-completion"] = "[DB]",
+          calc = "[Clc]",
+          emoji = "[Emj]",
+        })[entry.source.name]
+        return vim_item
+      end,
     },
-    mapping = cmp.mapping.preset.insert({
-
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ["<Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif vim.api.nvim_get_mode().mode == "c" then
-                fallback()
-            else
-                fallback()
-            end
-        end,
-        ["<S-Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            elseif vim.api.nvim_get_mode().mode == "c" then
-                fallback()
-            else
-                fallback()
-            end
-        end,
-    }),
-    -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp_signature_help', keyword_length = 2 },
-        { name = 'nvim_lsp',                keyword_length = 3 },
-        { name = 'path',                    keyword_length = 2 },
-        { name = 'luasnip',                 keyword_length = 2 },
-        { name = 'nvim_lua',                keyword_length = 2 },
-    }, {
-        { name = 'buffer' },
-    })
-})
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert {
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif vim.api.nvim_get_mode().mode == "c" then
+        fallback()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      elseif vim.api.nvim_get_mode().mode == "c" then
+        fallback()
+      else
+        fallback()
+      end
+    end,
+  },
+  -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
+  sources = cmp.config.sources({
+    { name = "nvim_lsp_signature_help", keyword_length = 2 },
+    { name = "nvim_lsp", keyword_length = 3 },
+    { name = "path", keyword_length = 2 },
+    { name = "luasnip", keyword_length = 2 },
+    { name = "nvim_lua", keyword_length = 2 },
+  }, {
+    { name = "buffer" },
+  }),
+}
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-        { name = 'buffer' },
-    })
+cmp.setup.filetype("gitcommit", {
+  sources = cmp.config.sources({
+    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = "buffer" },
+  }),
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
 
 cmp.setup.filetype({ "markdown", "vimwiki" }, {
