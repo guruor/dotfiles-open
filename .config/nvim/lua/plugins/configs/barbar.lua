35451ab3d6448751dc0e4a0bdf3f68f9158271b6
G0V1NDS <connect.govinds@gmail.com>
2023-05-20 23:16:43 +0530

    - Corrected close button for active tab - Disabled error diagnostic for tabline - Disabled separator for inactive tabs - Replaced active separator with a `.` char

--- .config/nvim/lua/plugins/configs/barbar.lua
+++ .config/nvim/lua/plugins/configs/barbar.lua
@@ -38,13 +38,13 @@ require'barbar'.setup {
     -- Configure the base icons on the bufferline.
     buffer_index = false,
     buffer_number = false,
-    button = '',
+    button = "×",
     -- Enables / disables diagnostic symbols
     diagnostics = {
-      [vim.diagnostic.severity.ERROR] = {enabled = false, icon = ''},
       [vim.diagnostic.severity.WARN] = {enabled = false, icon = ''},
       [vim.diagnostic.severity.INFO] = {enabled = false, icon = ''},
       [vim.diagnostic.severity.HINT] = {enabled = false, icon = ''},
+      [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "" },
     },
     filetype = {
       -- Sets the icon's highlight group.
@@ -54,7 +54,7 @@ require'barbar'.setup {
       -- Requires `nvim-web-devicons` if `true`
       enabled = true,
     },
-    separator = {left = '▎', right = ''},
+    separator = { left = ".", right = "" },
 
     -- Configure the icons on the bufferline when modified or pinned.
     -- Supports all the base icon options.
@@ -65,8 +65,11 @@ require'barbar'.setup {
     -- Supports all the base icon options, plus `modified` and `pinned`.
     alternate = {filetype = {enabled = false}},
     current = {buffer_index = true},
-    inactive = {button = '×'},
     visible = {modified = {buffer_number = false}},
+    inactive = {
+      button = "×",
+      separator = { left = "", right = "" },
+    },
   },
 
   -- If true, new buffers will be inserted at the start/end of the list.
