29012b318469140a57ddd2e71d1c121eacc32b6c
G0V1NDS <connect.govinds@gmail.com>
2023-05-26 17:47:05 +0530

    - Logging window app and title

--- .config/yabai/scripts/signals/window_created.sh
+++ .config/yabai/scripts/signals/window_created.sh
@@ -4,4 +4,6 @@
 log_prefix="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 echo -e "[$log_prefix] YABAI_WINDOW_ID: $YABAI_WINDOW_ID"
+window=$(yabai -m query --windows --window "$YABAI_WINDOW_ID")
+echo -e "[$log_prefix] Window info: $(echo "$window" | jq 'with_entries(select([.key] | inside(["app", "title"])))')"
 # echo -e "[$log_prefix] Window info: $(yabai -m query --windows --window "$YABAI_WINDOW_ID")"
