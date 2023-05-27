2d6137136b63837a0f1408ceb85050c14d12d789
G0V1NDS <connect.govinds@gmail.com>
2023-05-27 15:28:01 +0530

    Removed the background for FloatBorder and NormalFloat

--- .config/nvim/lua/plugins/configs/colorscheme.lua
+++ .config/nvim/lua/plugins/configs/colorscheme.lua
@@ -33,6 +33,8 @@ vim.cmd('colorscheme gruvbox-material')
 -- No highlight for window separator, userful with global statusline
 -- highlight WinSeparator guibg=None
 vim.api.nvim_set_hl(0, 'WinSeparator', { guibg = nil })
+vim.api.nvim_set_hl(0, 'FloatBorder', { guibg = nil })
+vim.api.nvim_set_hl(0, 'NormalFloat', { guibg = nil })
 
 -- Turns off highlighting on the bits of code that are changed, so the line that is
 -- changed is highlighted but the actual text that has changed stands out on the line and is readable.
