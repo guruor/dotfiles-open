2c59ee698cdfb3e790f8a47169219d7cab680337
G0V1NDS <connect.govinds@gmail.com>
2023-05-27 15:29:12 +0530

    Enable update check and notification

--- .config/nvim/lua/plugins/configs/lazy_nvim.lua
+++ .config/nvim/lua/plugins/configs/lazy_nvim.lua
@@ -13,6 +13,14 @@ return {
     },
   },
 
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+  },
+
   performance = {
     rtp = {
       disabled_plugins = {
