9afcdbf957ec78fbdce12134ea1ba7cf2a032a5d
G0V1NDS <connect.govinds@gmail.com>
2023-10-29 23:10:40 +0530

    Changed the default font to fisa code, Fira Code + Operator Mono = Fisa Code a patched font from icursive-nerd-font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   color_scheme = "Gruvbox dark, soft (base16)",
-  font = wezterm.font 'Fira Code',
+  font = wezterm.font 'Fisa Code',
   font_size = 14,
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "NONE"
