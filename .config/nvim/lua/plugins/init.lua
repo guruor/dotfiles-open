efb23d86150eea88feea305cef9e9fe8464437cf
G0V1NDS <connect.govinds@gmail.com>
2023-11-03 10:22:01 +0530

    Lazy loading gitsigns and loads only when a git file is open

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -129,9 +129,27 @@ local default_plugins = {
   },
   {
     "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
-    config = function()
-      require "plugins.configs.gitsigns"
+    ft = { "gitcommit", "diff" },
+    init = function()
+      -- load gitsigns only when a git file is opened
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+            vim.schedule(function()
+              require("lazy").load { plugins = { "gitsigns.nvim" } }
+            end)
+          end
+        end,
+      })
+    end,
+    opts = function()
+      return require("plugins.configs.gitsigns")
+    end,
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
     end,
   },
   { "sindrets/diffview.nvim",
