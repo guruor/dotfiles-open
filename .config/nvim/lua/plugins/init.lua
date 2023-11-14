1a7b4991739f40676623fd08ac212bd081c14405
G0V1NDS <connect.govinds@gmail.com>
2023-11-14 16:21:35 +0530

    Replaced vim-fubitive, fugitive-gitlab and vim-rhubarb with linrongbin16/gitlinker.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -116,11 +116,30 @@ local default_plugins = {
       "GBrowse",
     },
     dependencies = {
-      "tommcdo/vim-fubitive", -- GBrowse bitbucket support
-      "shumphrey/fugitive-gitlab.vim", -- Gitlab gitlab support
-      "tpope/vim-rhubarb", -- GBrowse github support
+      -- "ruifm/gitlinker.nvim", -- Similar functionality like fugitive GBrowse
+      "linrongbin16/gitlinker.nvim", -- Fork for ruifm/gitlinker.nvim
     },
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+		lazy = false,
+    init = function()
+      -- load gitlinker only when a git file is opened
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitLinkerLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name "GitLinkerLazyLoad"
+            vim.schedule(function()
+              local opts = require("plugins.configs.misc").gitlinker()
+              require("gitlinker").setup(opts)
+            end)
+          end
+        end,
+      })
+    end,
+  }, -- Similar functionality like fugitive GBrowse
   {
     "lewis6991/gitsigns.nvim",
     ft = { "gitcommit", "diff" },
