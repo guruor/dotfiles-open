46344d70c935632da26259c6df335d59486c6d4e
G0V1NDS <connect.govinds@gmail.com>
2023-11-18 14:45:05 +0530

    Updated gitlinker config, lazy loading on GitLink cmd

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -113,32 +113,13 @@ local default_plugins = {
       "GMove",
       "GDelete",
       "GRemove",
-      "GBrowse",
-    },
-    dependencies = {
-      -- "ruifm/gitlinker.nvim", -- Similar functionality like fugitive GBrowse
-      "linrongbin16/gitlinker.nvim", -- Fork for ruifm/gitlinker.nvim
     },
   },
   {
     "linrongbin16/gitlinker.nvim",
-		lazy = false,
-    init = function()
-      -- load gitlinker only when a git file is opened
-      vim.api.nvim_create_autocmd({ "BufRead" }, {
-        group = vim.api.nvim_create_augroup("GitLinkerLazyLoad", { clear = true }),
-        callback = function()
-          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
-          if vim.v.shell_error == 0 then
-            vim.api.nvim_del_augroup_by_name "GitLinkerLazyLoad"
-            vim.schedule(function()
-              local opts = require("plugins.configs.misc").gitlinker()
-              require("gitlinker").setup(opts)
-            end)
-          end
-        end,
-      })
-    end,
+    -- dir = "~/Workspace/vim-plugins/gitlinker.nvim", -- Fork for ruifm/gitlinker.nvim
+    cmd = { "GitLink" },
+    config = true,
   }, -- Similar functionality like fugitive GBrowse
   {
     "lewis6991/gitsigns.nvim",
