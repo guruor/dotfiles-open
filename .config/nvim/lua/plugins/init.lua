42af0d3452432931e806179fe9b23e31314c389d
G0V1NDS <connect.govinds@gmail.com>
2023-11-20 00:25:27 +0530

    Added flash.nvim as alternative to leap.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -483,11 +483,17 @@ local default_plugins = {
     end,
   },
   {
-    "ggandor/leap.nvim",
-    keys = {"s", "S"},
-    config = function()
-      require("leap").set_default_keymaps()
-    end,
+    "folke/flash.nvim", -- leap.nvim alternative
+    event = "VeryLazy",
+    config = true,
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
   {
     "akinsho/toggleterm.nvim",
