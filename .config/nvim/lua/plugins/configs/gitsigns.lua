7d90c9e1641633b3eba2e9978630e8dc37926f2f
G0V1NDS <connect.govinds@gmail.com>
2023-11-21 10:48:29 +0530

    - Removed all gitsigns related mapping from whichkey - Using same gitsings mapping as LazyVim

--- .config/nvim/lua/plugins/configs/gitsigns.lua
+++ .config/nvim/lua/plugins/configs/gitsigns.lua
@@ -33,45 +33,27 @@ M.options = {
   yadm = {
     enable = false
   },
-  on_attach = function(bufnr)
+  on_attach = function(buffer)
     local gs = package.loaded.gitsigns
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
+    local function map(mode, l, r, desc)
+      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
     end
 
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
-
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
-
-    -- Actions
-    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>ha', gs.stage_hunk)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
-
-    -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
+    -- stylua: ignore start
+    map("n", "]h", gs.next_hunk, "Next Hunk")
+    map("n", "[h", gs.prev_hunk, "Prev Hunk")
+    map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+    map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+    map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+    map("n", "<leader>ghd", gs.diffthis, "Diff This")
+    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+  end,
 }
 
 return M
