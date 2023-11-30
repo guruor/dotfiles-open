63e55a0dee97973106b1d6366d2446bfaf854c14
G0V1NDS <connect.govinds@gmail.com>
2023-11-30 20:01:07 +0530

    Added mini.indentscope to highlight current scope, it is far better appealing than indent-blankline.nvim scope underline

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -314,7 +314,36 @@ local default_plugins = {
     end,
     event = { "BufReadPost" },
   },
-
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "VeryLazy", -- LazyFile
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
   { "mbbill/undotree", cmd = "UndotreeToggle" },
   "tpope/vim-repeat",
   {
