b91fcc2711eb2c054547832d2e2778ef924ab581
G0V1NDS <connect.govinds@gmail.com>
2023-12-12 17:36:48 +0530

    Added comment mentioning image.nvim works only with lua 5.1

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -694,6 +694,7 @@ local default_plugins = {
   -- "metakirby5/codi.vim",
   -- { "junegunn/vim-easy-align", run = { "EasyAlign" } },
   {
+    -- https://github.com/3rd/image.nvim/issues/91, works only with luarocks 5.1
     "3rd/image.nvim",
     ft = { "markdown", "vimwiki", "norg" },
     dependencies = "nvim-treesitter/nvim-treesitter",
