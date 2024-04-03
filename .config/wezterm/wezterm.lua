1d2196282bd33d25f0c6358843e2c0a156a7e995
G0V1NDS <connect.govinds@gmail.com>
2024-04-03 17:26:19 +0530

    Formatted the config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  -- color_scheme = "Gruvbox dark, soft (base16)",
-  color_scheme = "GitHub Dark",
-  font = wezterm.font 'Fisa Code',
-  font_size = 14,
-  hide_tab_bar_if_only_one_tab = true,
-  window_decorations = "NONE"
+    -- color_scheme = "Gruvbox dark, soft (base16)",
+    default_prog = { "/bin/zsh", "--login" },
+    color_scheme = "GitHub Dark",
+    font = wezterm.font("Fisa Code"),
+    font_size = 14,
+    hide_tab_bar_if_only_one_tab = true,
+    window_decorations = "NONE",
 }
