78acaa8cdee1fe04fc98cda6961849d614efe5b3
G0V1NDS <connect.govinds@gmail.com>
2024-04-03 08:31:54 +0530

    Changed the default theme to GitHub Dark

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require 'wezterm'
 
 return {
-  color_scheme = "Gruvbox dark, soft (base16)",
+  -- color_scheme = "Gruvbox dark, soft (base16)",
+  color_scheme = "GitHub Dark",
   font = wezterm.font 'Fisa Code',
   font_size = 14,
   hide_tab_bar_if_only_one_tab = true,
