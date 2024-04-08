40c82ac168557d056a72bb118c3918348384179b
G0V1NDS <connect.govinds@gmail.com>
2024-04-08 20:42:49 +0530

    Fixed issue with wezterm not being resizable and controlled by yabai

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,5 +7,13 @@ return {
     font = wezterm.font("Fisa Code"),
     font_size = 14,
     hide_tab_bar_if_only_one_tab = true,
-    window_decorations = "NONE",
+    -- NONE | TITLE | RESIZE
+    window_decorations = "RESIZE",
+    window_padding = {
+      left = 0,
+      right = 0,
+      top = 0,
+      bottom = 0
+    }
+
 }
