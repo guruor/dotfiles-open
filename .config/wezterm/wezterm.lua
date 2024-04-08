ddfd9137eb89dadbbf6b80ff4c19651265c9aacd
G0V1NDS <connect.govinds@gmail.com>
2024-04-08 22:52:13 +0530

    Disable hot reloading

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,19 +1,19 @@
 local wezterm = require("wezterm")
-
-return {
-    -- color_scheme = "Gruvbox dark, soft (base16)",
-    default_prog = { "/bin/zsh", "--login" },
-    color_scheme = "GitHub Dark",
-    font = wezterm.font("Fisa Code"),
-    font_size = 14,
-    hide_tab_bar_if_only_one_tab = true,
-    -- NONE | TITLE | RESIZE
-    window_decorations = "RESIZE",
-    window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0
-    }
-
+local config = wezterm.config_builder()
+-- color_scheme = "Gruvbox dark, soft (base16)",
+config.default_prog = { "/bin/zsh", "--login" }
+config.color_scheme = "GitHub Dark"
+config.font = wezterm.font("Fisa Code")
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+-- NONE | TITLE | RESIZE
+config.window_decorations = "RESIZE"
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
+config.automatically_reload_config = false
+
+return config
