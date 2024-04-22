6b1b91791000e457ca3a4abfedec927c509bf692
G0V1NDS <connect.govinds@gmail.com>
2024-04-22 08:57:08 +0530

    Opening files in new buffer instead of new tab

--- .config/nvim/lua/plugins/configs/fzf.lua
+++ .config/nvim/lua/plugins/configs/fzf.lua
@@ -137,7 +137,7 @@ require 'fzf-lua'.setup {
             -- replace the default action with the below
             -- to open all files whether single or multiple
             -- ["default"]     = actions.file_edit,
-            ["default"] = actions.file_tabedit,
+            ["default"] = actions.file_edit,
             ["ctrl-s"]  = actions.file_split,
             ["ctrl-v"]  = actions.file_vsplit,
             ["ctrl-t"]  = actions.file_tabedit,
