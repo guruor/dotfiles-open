f05b4b0a9fd49fe7f09f2f20cd7e320c886642d1
G0V1NDS <connect.govinds@gmail.com>
2024-05-18 15:37:43 +0530

    Using nvim-lint instead of none-ls

--- .config/nvim/lua/lsp/none-ls.lua
+++ .config/nvim/lua/lsp/none-ls.lua
@@ -22,16 +22,7 @@ null_ls.setup {
     --   extra_args = { "--config", "auto" },
     -- },
 
-    -- Fast linter and formatter for python
-    -- Yet to add all the pylint rules in it, it is progressing fast
-    require "none-ls.diagnostics.ruff",
-    -- Detects unused python code
-    -- diagnostics.vulture,
-
     -- diagnostics.staticcheck,
-    -- diagnostics.golangci_lint,
-    -- revive is not needed when we have golangci_lint
-    -- diagnostics.revive,
     -- diagnostics.gospel,
     -- code_actions.gomodifytags,
     -- code_actions.impl,
@@ -39,20 +30,7 @@ null_ls.setup {
     -- require("go.null_ls").gotest_action(),
     -- require("go.null_ls").golangci_lint(),
 
-    -- Faster alternative to eslint for JS and TS
-    -- diagnostics.deno_lint,
-    require "none-ls.diagnostics.eslint_d",
     -- code_actions.eslint_d,
-    -- diagnostics.typos.with {
-    --   filetypes = { "markdown", "vimwiki" },
-    -- },
-
-    -- Spell and grammer related LSPs
-    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md#vale
-    -- Faster alternative to textlint and proselint
-    diagnostics.vale.with {
-      extra_filetypes = { "vimwiki" },
-    },
     -- LanguageTool rust client, which gets suggestions from LanguageTool server
     -- https://github.com/jeertmans/languagetool-rust
     -- diagnostics.ltrs.with {
@@ -74,6 +52,5 @@ null_ls.setup {
     -- OpenAPI linter for yaml and json
     diagnostics.vacuum,
 
-    -- diagnostics.dotenv_linter,
   },
 }
