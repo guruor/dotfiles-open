0404cdf95247a924759e60b19b38c2f04e7d4217
guruor <connect.govinds@gmail.com>
2024-06-05 08:43:43 +0530

    Replaced neodev.nvim with lazydev.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -39,10 +39,17 @@ local lsp_plugins = {
   },
   -- Managing and installing LSP servers
   {
-    'folke/neodev.nvim',
-    ft = { 'lua', 'vim' },
-    config = true
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
   },
+  { "Bilal2453/luvit-meta", lazy = true }, -- `vim.uv` typings
   -- Explore https://github.com/hinell/lsp-timeout.nvim
   {
     "neovim/nvim-lspconfig",
