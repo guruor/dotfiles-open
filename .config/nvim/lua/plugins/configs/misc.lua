f001eef8f6f9c903d280a81b23cc6b90af8be9c9
guruor <connect.govinds@gmail.com>
2024-06-10 01:18:28 +0530

    Disabled dap support from go.nvim

--- .config/nvim/lua/plugins/configs/misc.lua
+++ .config/nvim/lua/plugins/configs/misc.lua
@@ -152,6 +152,7 @@ M.dressing = {
 }
 
 M.go = {
+  disable_defaults = true,
   goimports = "gopls", -- if set to 'gopls' will use golsp format
   gofmt = "gopls", -- if set to gopls will use golsp format
   max_line_len = 120,
@@ -161,7 +162,7 @@ M.go = {
   lsp_cfg = false, -- false: use your own lspconfig
   lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
   lsp_on_attach = false, -- use on_attach from go.nvim
-  dap_debug = true,
+  dap_debug = false,
   lsp_inlay_hints = {
     enable = false,
   }
