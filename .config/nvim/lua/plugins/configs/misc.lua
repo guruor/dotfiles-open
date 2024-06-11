981aed184cae177030b3056ecfd458970b913e78
guruor <connect.govinds@gmail.com>
2024-06-11 15:21:38 +0530

    Replaced go.nvim with lightweight nvim-go for go tools support

--- .config/nvim/lua/plugins/configs/misc.lua
+++ .config/nvim/lua/plugins/configs/misc.lua
@@ -151,21 +151,11 @@ M.dressing = {
   },
 }
 
-M.go = {
-  disable_defaults = true,
-  goimports = "gopls", -- if set to 'gopls' will use golsp format
-  gofmt = "gopls", -- if set to gopls will use golsp format
-  max_line_len = 120,
-  tag_transform = false,
-  test_dir = "",
-  comment_placeholder = "   ",
-  lsp_cfg = false, -- false: use your own lspconfig
-  lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
-  lsp_on_attach = false, -- use on_attach from go.nvim
-  dap_debug = false,
-  lsp_inlay_hints = {
-    enable = false,
-  }
+M.nvimGo = {
+  notify = true,
+  auto_format = false,
+  auto_lint = false,
+  linter = 'golangci-lint',
 }
 
 M.noice = {
