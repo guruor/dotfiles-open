4e451e485c9ed5bd588925311bf8cc3ee3092083
guruor <connect.govinds@gmail.com>
2024-06-13 16:08:16 +0530

    Calling lualine setup in init.lua

--- .config/nvim/lua/plugins/configs/statusline.lua
+++ .config/nvim/lua/plugins/configs/statusline.lua
@@ -1,9 +1,9 @@
-require('lualine').setup {
+return {
   options = {
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -15,22 +15,25 @@ require('lualine').setup {
       statusline = 1000,
       tabline = 1000,
       winbar = 1000,
-    }
+    },
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = { "mode" },
     lualine_b = {
-            'branch', 'diff',
-            {
-                'diagnostics',
-                on_click=function() vim.diagnostic.setqflist() end
-            },
-            {
-                GetEnvironmentName,
-                icon = 'Σ',
-                on_click=SelectEnvironmentForFiletype
-            }
-        },
+      "branch",
+      "diff",
+      {
+        "diagnostics",
+        on_click = function()
+          vim.diagnostic.setqflist()
+        end,
+      },
+      {
+        GetEnvironmentName,
+        icon = "Σ",
+        on_click = SelectEnvironmentForFiletype,
+      },
+    },
     lualine_c = {},
     lualine_x = {
       {
@@ -38,23 +41,23 @@ require('lualine').setup {
         cond = require("lazy.status").has_updates,
         color = { fg = "#ff9e64" },
       },
-      'encoding',
-      'fileformat',
-      'filetype',
+      "encoding",
+      "fileformat",
+      "filetype",
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {},
-    lualine_x = {'location'},
+    lualine_x = { "location" },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = { 'quickfix', 'fugitive', 'fzf' },
+  extensions = { "quickfix", "fugitive", "fzf" },
 }
