42dfba055d8c8290d21c358629afdd1771b054a4
guruor <connect.govinds@gmail.com>
2024-06-22 15:49:07 +0530

    Added lsp-timeout.nvim to auto restart or kill inactive LSP servers

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,23 @@
 return {
   -- Explore https://github.com/hinell/lsp-timeout.nvim
   {
+    "hinell/lsp-timeout.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    event = "VeryLazy",
+    init = function()
+      vim.g.lspTimeoutConfig = {
+        stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs
+        startTimeout = 1000 * 10, -- ms, timeout before restart
+        silent = false, -- true to suppress notifications
+        filetypes = {
+          ignore = { -- filetypes to ignore; empty by default
+            -- lsp-timeout is disabled completely
+          }, -- for these filetypes
+        },
+      }
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       { "simrat39/rust-tools.nvim", ft = "rs" },
