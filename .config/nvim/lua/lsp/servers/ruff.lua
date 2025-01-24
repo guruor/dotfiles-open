85b9e7c442860603627bc00b3fb014737d7b2afd
guruor <connect.govinds@gmail.com>
2025-01-24 18:49:04 +0530

    Started using lspserver from ruff instead of ruff_lsp Replaced tsserver with ts_ls

--- .config/nvim/lua/lsp/servers/ruff.lua
+++ .config/nvim/lua/lsp/servers/ruff.lua
@@ -7,8 +7,8 @@ local function on_attach_func_without_pyright_features(client)
   require("lsp.utils").default_on_attach_func(client)
 end
 
--- Configure `ruff-lsp`.
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- Configure `ruff`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff
 -- For the default config, along with instructions on how to customize the settings
 M.on_attach = on_attach_func_without_pyright_features
 
