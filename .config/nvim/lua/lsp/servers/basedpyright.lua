7fd66d5444ee9dc16ff0c060d0ef98003c2103e1
guruor <connect.govinds@gmail.com>
2025-03-16 11:14:42 +0530

    Added basedpyright config, disabled pyright

--- .config/nvim/lua/lsp/servers/basedpyright.lua
+++ .config/nvim/lua/lsp/servers/basedpyright.lua
@@ -4,21 +4,17 @@ local M = utils.merge({}, require "lsp.servers.common")
 -- https://github.com/golang/tools/tree/master/gopls
 M.on_attach = require("lsp.utils").default_on_attach_func_without_formatting_support
 M.settings = {
-  pyright = {
+  basedpyright = {
     -- Using Ruff's import organizer
     disableOrganizeImports = true,
     disableTaggedHints = true,
-  },
-  python = {
     analysis = {
-      -- Ignore all files for analysis to exclusively use Ruff for linting
-      -- ignore = { "*" },
-      diagnosticSeverityOverrides = {
-        -- https://github.com/microsoft/pyright/blob/main/docs/configuration.md#type-check-diagnostics-settings
-        reportUndefinedVariable = "none",
-      },
+      typeCheckingMode = "standard",
+      diagnosticMode = "openFilesOnly",
+      useLibraryCodeForTypes = true,
     },
   },
+  python = {},
 }
 
 return M
