614adfc3c7a3738beef9d6514ffdf356199b4514
guruor <connect.govinds@gmail.com>
2025-03-16 08:11:41 +0530

    Disabled lazy loading for dropbar and added mapping to jump to symbols

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -95,6 +95,15 @@ return {
     -- IDE-like breadcrumbs
     "Bekaboo/dropbar.nvim",
     event = "BufRead",
+    -- Lazy loading is already handled by plugin
+    -- https://github.com/Bekaboo/dropbar.nvim/blob/master/plugin/dropbar.lua
+    lazy = false,
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
   },
   {
     -- For code outline window
