83a850c10a8bc536918dcb760ed56e76f89220e6
guruor <connect.govinds@gmail.com>
2025-04-12 02:30:31 +0530

    Added git symbols and color

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,17 @@
-require("git"):setup({ order = 0 })
 require("full-border"):setup()
 require("session"):setup({
     sync_yanked = true,
 })
+
+th.git = th.git or {}
+th.git.ignored_sign = " " -- Gitignored
+th.git.untracked_sign = "?"
+th.git.modified_sign = "M"
+th.git.added_sign = "A"
+th.git.deleted_sign = "D"
+th.git.updated_sign = "U"
+
+th.git.modified = ui.Style():fg("blue")
+th.git.added = ui.Style():fg("green")
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.updated = ui.Style():fg("green")
