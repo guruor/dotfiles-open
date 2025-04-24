8baa027726f53869609816c5afcc7c976a2b09fb
guruor <connect.govinds@gmail.com>
2025-04-24 17:52:22 +0530

    Added biome formatter for multiple filetypes

--- .config/nvim/lua/plugins/configs/conform.lua
+++ .config/nvim/lua/plugins/configs/conform.lua
@@ -8,6 +8,30 @@
 -- https://github.com/lukas-reineke/cbfmt/issues/37
 --
 -- List of supported formatters: https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
+--
+-- https://biomejs.dev/internals/language-support/
+local biome_supported_fts = {
+  "astro",
+  "css",
+  "graphql",
+  "javascript",
+  "javascriptreact",
+  "json",
+  "jsonc",
+  "svelte",
+  "typescript",
+  "typescriptreact",
+  "vue",
+}
+
+local function set_biome_formatter(formatters_by_ft)
+  for _, ft in ipairs(biome_supported_fts) do
+    formatters_by_ft[ft] = formatters_by_ft[ft] or {}
+    table.insert(formatters_by_ft[ft], "biome")
+    table.insert(formatters_by_ft[ft], "biome-organize-imports")
+  end
+end
+
 local function set_sql_formatter()
   local cwd = vim.fn.getcwd()
   local cwd_final_name = cwd:match "([^/]+)$"
@@ -33,18 +57,13 @@ local formatters_by_ft = {
   python = { "ruff_format", "ruff_fix" },
   -- As of now biome supports only js, ts and json
   -- https://biomejs.dev/internals/language-support/
-  json = { "biome", "dprint", "prettierd", "yq", stop_after_first = true },
-  javascript = { "biome", "biome-organize-imports" },
-  typescript = { "biome", "biome-organize-imports" },
-  typescriptreact = { "biome", "biome-organize-imports" },
-  javascriptreact = { "biome", "biome-organize-imports" },
+  -- javascript = { "biome", "biome-organize-imports", "dprint", "prettierd", "prettier", stop_after_first = true },
   rust = { "rustfmt" },
   sh = { "shfmt" },
   bash = { "shfmt" },
   zsh = { "shfmt" },
   cmake = { "cmake_format" },
-  css = { "dprint", "prettierd" },
-  html = { "dprint", "prettierd" },
+  -- html = { "dprint", "prettierd" },
   toml = { "taplo" },
   terraform = { "terraform_fmt" },
   yaml = { "yamlfmt", "dprint", "prettierd", "yq", stop_after_first = true },
@@ -56,7 +75,7 @@ local formatters_by_ft = {
 }
 
 formatters_by_ft["sql"] = set_sql_formatter(vim.fn.bufnr())
-P(formatters_by_ft)
+set_biome_formatter(formatters_by_ft)
 
 return {
   formatters_by_ft = formatters_by_ft,
