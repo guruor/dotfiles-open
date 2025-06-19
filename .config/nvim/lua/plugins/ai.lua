df8c8bd18b4081c8b0c86a432f836f34f94d3445
guruor <connect.govinds@gmail.com>
2025-06-19 23:57:33 +0530

    Fixes issue with codecompanion extention config Adds VectorCode extension

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -9,12 +9,22 @@ return {
     end,
   },
   {
+    -- Install the `vectorcode` first: uv tool install vectorcode
+    "Davidyz/VectorCode",
+    version = "*", -- optional, depending on whether you're on nightly or release
+    build = "uv upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    cmd = { "VectorCode" },
+  },
+  {
     "olimorris/codecompanion.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ibhagwan/fzf-lua",
       "ravitemer/mcphub.nvim",
+      "Davidyz/VectorCode",
       "ravitemer/codecompanion-history.nvim",
     },
     cmd = {
@@ -26,31 +36,39 @@ return {
     },
     -- Default config: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
     opts = {
-      default_adapter = "openrouter",
+      default_adapter = "copilot",
       display = {
         chat = {
           auto_scroll = false,
         },
       },
-    },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          show_result_in_chat = true, -- Show mcp tool results in chat
-          make_vars = true, -- Convert resources to #variables
-          make_slash_commands = true, -- Add prompts as /slash commands
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true, -- Show mcp tool results in chat
+            make_vars = true, -- Convert resources to #variables
+            make_slash_commands = true, -- Add prompts as /slash commands
+          },
+        },
+        history = {
+          enabled = true,
+          opts = {
+            keymap = "gh",
+            save_chat_keymap = "sc",
+            auto_save = true,
+          },
+        },
+        vectorcode = {
+          opts = {
+            add_tool = true,
+          },
         },
       },
-      history = {
-        enabled = true,
-        opts = {
-          keymap = "gh",
-          save_chat_keymap = "sc",
-          auto_save = true,
-        },
-      },
     },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+    end,
     keys = { "<leader>a" },
   },
   {
